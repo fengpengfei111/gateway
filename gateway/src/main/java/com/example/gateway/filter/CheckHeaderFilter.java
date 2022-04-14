0817d36f5c6b2f9c928baf0094e1b2e1a7a71283
fengpengfei111 <1479623700@qq.com>
2022-04-14 11:52:09 +0800

    push

--- gateway/src/main/java/com/example/gateway/filter/CheckHeaderFilter.java
+++ gateway/src/main/java/com/example/gateway/filter/CheckHeaderFilter.java
@@ -1,16 +1,27 @@
 package com.example.gateway.filter;
 
+import com.example.gateway.util.RedisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component
 public class CheckHeaderFilter implements GlobalFilter {
+    @Autowired
+    RedisUtil redisUtil;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        HttpHeaders httpHeaders = exchange.getRequest().getHeaders();
-        httpHeaders.add("gatewayPath", "true");
-        return chain.filter(exchange);
+        System.out.println(redisUtil.get("fromGateway"));
+        ServerHttpRequest req = exchange.getRequest().mutate()
+                .header("from", "gateway").build();
+        // 上游给Token   // auth服务解析Token
+        //restTe  Group
+        //contains 1  2  3  4  5
+        return chain.filter(exchange.mutate().request(req.mutate().build()).build());
     }
 }
